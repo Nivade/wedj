1ff761756a9164117e63a7de2c0a1b7ae84c2b83
Nivade <nhavandeursen@gmail.com>
2016-02-27 20:15:17 +0100

    Renamed playlist to search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,5 +1,114 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<html>
+	<head>
+		<title>Playlist</title>
 
-<div container="container">
+		<link href="{{ asset('css/search.scss') }}" media="all" rel="stylesheet" type="text/css" />
 
-</div>
+	</head>
+	<body>
+		<div id="container">
+			<div id="search">
+				<form>
+					<input id="search_song" type="text" name="search_song" placeholder="Search"/>
+				</form>
+			</div>
+			<div id="switch">
+				<input id="yt_switch" type="submit" name="yt_switch" value="YouTube"/>
+				<input id="sc_switch" type="submit" name="sc_switch" value="SoundCloud"/>
+			</div>
+			<div id="request_holder">
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+				<div class="song_request">
+					<img src="">
+					<div class="content">
+						<h3>Title</h3>
+						<p>Description</p>
+						<p>Duration</p>
+					</div>
+					<input id="add_button" type="submit" name="add_song_button" value="Add Song"/>
+				</div>
+			</div>
+
+		</div>
+	</body>
+</html>
